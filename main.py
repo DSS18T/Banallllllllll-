1700e8a8a8916145260ab13b4f9ba43727cb7b9f
Darsh <darshshetty04@gmail.com>
2023-05-07 17:19:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from RiZoeLX.functions import start_banall
+
+from pyrogram import Client, filters
+
+from pyrogram.types import Message
+
+@Client.on_message(filters.group & filters.command("banall"))
+
+async def banall_members(Client, Message):
+
+   await start_banall(Client, Message)
 
 
-f
 
 
 
